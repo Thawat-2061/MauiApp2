08a72fa58e3b5b789bc9b24c62e204761f40bf98
Thawat-2061 <65011212061@msu.ac.th>
2025-03-28 16:08:46 +0700

    new2

--- MauiApp2/obj/Debug/net9.0-windows10.0.19041.0/win10-x64/MauiApp2.AssemblyInfo.cs
+++ MauiApp2/obj/Debug/net9.0-windows10.0.19041.0/win10-x64/MauiApp2.AssemblyInfo.cs
@@ -17,7 +17,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MauiApp2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+a2f2328317554d76da35a66b233aabb155795358")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+df09cf2e27b57cd27d62a2ca74f4fae454ef4af1")]
 [assembly: System.Reflection.AssemblyProductAttribute("MauiApp2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MauiApp2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
